80936e824a5fe99cbb81504c98f8f8c35b61e0e2
pranav <pranavkumar456n@outlook.com>
2023-03-28 11:32:47 +0530

    28/3/23

--- language/java/h9simplecal.java
+++ language/java/h9simplecal.java
@@ -3,14 +3,7 @@ public class h9simplecal {
     public static void main(String []agrs){
         try(Scanner sc = new Scanner(System.in)){
             System.out.print("Enter the value 1 = ");
-            int a = sc.nextInt();
-            System.out.print("Enter the value 2 = ");
-            int b = sc.nextInt();
             
-            
-            if(){
-
-            }
         }
     }
 }
