14e03c35df38f2c25c6eabf0b5bfb82b4aaf3e96
pranav <pranavkumar456n@outlook.com>
2023-03-30 10:01:17 +0530

    30/3/23

---
+++ language/java/Question java/Q22Amstrong.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Q22Amstrong {
+    public static void main(String []agrs){
+        Scanner dc =new Scanner(System.in);
+        int number = dc.nextInt();
+        
+    }    
+}
