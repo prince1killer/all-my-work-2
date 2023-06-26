17cb8d12491aa2ef7093116a881515a211beb822
pranav <pranavkumar456n@outlook.com>
2023-06-26 18:09:49 +0530

    26/6/23

--- language/nodejs/learn-nodejs/api/learnjson.js
+++ language/nodejs/learn-nodejs/api/learnjson.js
@@ -10,15 +10,16 @@ const info = {
     phone : 123123123
 }
 // console.log(info)
-const newJson = JSON.stringify(info)
-console.log(newJson)
-// console.log(fs.writeFile("language/nodejs/learn-nodejs/api/new.json",newJson,(err)=>{
-//     console.log(err)
-// }))
+// const newJson = JSON.stringify(info)
+// console.log(newJson)
+console.log(fs.writeFile("language/nodejs/learn-nodejs/api/new.json",newJson,(err)=>{
+    console.log(err)
+}))
 let newObj = JSON.parse(newJson);
 console.log(newObj)
 
-console.log(fs.readFile("language/nodejs/learn-nodejs/api/new.json"),(err,data)=>{
-    console.log(err);
+fs.readFile("./language/nodejs/learn-nodejs/api/new.json","utf-8",(err,data)=>{
     console.log(data);
-})
+    const onj = JSON.parse(data)
+    console.log(onj)
+})  
