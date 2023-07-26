fe4ac8a64a2357e84e596829207eee694c166d52
pranav <pranavkumar456n@outlook.com>
2023-07-26 22:53:15 +0530

    26/07/23

---
+++ asp.net/asp.net learn/admain/login/registration.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.Common;
+
+public partial class login_registration : System.Web.UI.Page
+{
+    SqlConnection con;
+    SqlCommand cmd;
+    DataAdapter da;
+    SqlDataAdapter ds;
+
+
+    void myReg() {
+        con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|Datadirectory|product_data.mdf;Integrated Security=True");
+        con.Open();
+    }
+
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btn_Click(object sender, EventArgs e)
+    {
+
+
+        if ((FileUpload1.PostedFile != null) && (FileUpload1.PostedFile.ContentLength > 0)) { 
+            string fn = Path.GetFileName(FileUpload1.PostedFile.FileName);
+            string SaveLocation = Server.MapPath("../Image/")+fn;
+            FileUpload1.PostedFile.SaveAs(SaveLocation);
+            Image1.ImageUrl = "../Image/" + fn;
+            Response.Write("<script>alert('successfully upload');</script>");
+        }
+
+    }
+
+    protected void FileUpload1_Load(object sender, EventArgs e)
+    {
+    }
+}
