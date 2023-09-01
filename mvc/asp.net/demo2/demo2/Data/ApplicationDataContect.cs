6bf97969871c682f0dd0fa9b884e2c218b8c3230
omprince <pranavkumar0234@gmail.com>
2023-09-01 11:42:47 +0530

    1/9/2023

---
+++ mvc/asp.net/demo2/demo2/Data/ApplicationDataContect.cs
@@ -0,0 +1,15 @@
+﻿using demo2.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace demo2.Data
+{
+    public class ApplicationDataContect:DbContext
+    {
+        public ApplicationDataContect(DbContextOptions<ApplicationDataContect> options ) : base(options)
+        {
+        
+        }
+        public DataSet<Category>
+    }
+}
